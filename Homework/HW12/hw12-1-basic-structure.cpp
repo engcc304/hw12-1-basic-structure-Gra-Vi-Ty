75720002ac8331175c4db7c964c740cbe703d3e6
Gra-Vi-Ty <worakan223@gmail.com>
2023-10-11 20:06:18 +0700

    uppppp

--- Homework/HW12/hw12-1-basic-structure.cpp
+++ Homework/HW12/hw12-1-basic-structure.cpp
@@ -40,3 +40,51 @@
         Score : 87.60 points
 
 */
+#include <stdio.h>
+#include <string.h>
+
+struct Person {
+	    char  Name[20] ;
+	    int   Age ;
+	    char  Sex ;
+	    float Score ;
+	} ;
+
+int main() {
+    
+    struct Person A , B ;
+
+    printf( "Person A\n" ) ;
+    printf( "Name : " ) ;
+    scanf( "%s" , A.Name ) ;
+    printf( "Age : " ) ;
+    scanf( "%d" , &A.Age ) ;
+    printf( "Sex : " ) ;
+    scanf( " %c" , &A.Sex ) ;
+    printf( "Score : " ) ;
+    scanf( "%f" , &A.Score ) ;
+
+
+    printf( "Person B" ) ;
+    printf( "Name : " ) ;
+    scanf( "%s" , B.Name ) ;
+    printf( "Age : " ) ;
+    scanf( "%d" , &B.Age ) ;
+    printf( "Sex : " ) ;
+    scanf( " %c" , &B.Sex ) ;
+    printf( "Score : " ) ;
+    scanf( "%f" , &B.Score ) ;
+
+
+    printf( "--| Person A Information |--\n" ) ;
+    printf( "Name : %s ( %c )\n" ,A.Name ,&A.Sex) ;
+    printf( "Age : %d years old\n" ,A.Age ) ;
+    printf( "Score : %.2f points\n" , A.Score ) ;
+
+    printf( "--| Person b Information |--\n" ) ;
+    printf( "Name : %s ( %c )\n" ,B.Name ,&B.Sex) ;
+    printf( "Age : %d years old\n" ,B.Age ) ;
+    printf( "Score : %.2f points\n" ,B.Score ) ;
+
+    return 0 ;
+}
